6f58efa3a4d0059e989adb219f51c8240e706532
Werner Kotze <werner.kotze@clevva.com>
2019-07-16 19:39:37 +0200

    temp commmit

--- src/components/dashboard.vue
+++ src/components/dashboard.vue
@@ -9,79 +9,65 @@
                 fluid
                 grid-list-sm
               >
-                <v-layout row wrap>
-                  <v-flex
-                    v-for="hairdresser in nearbyHairdressers" 
-                    :key="hairdresser.creatorId"
-                  >
-                    <v-card>
-                      <v-img
-                        src="http://wallpaperstock.net/the-beatles-minimalistic-illustration_wallpapers_43830_1600x1200.jpg"
-                        height="200px"
-                      >
-                        <v-container
-                          fill-height
-                          fluid
-                          pa-2
+              <googlemaps-map
+                class="map"
+                :center.sync="center"
+                :zoom="12"
+                
+              >
+                <!-- User Position -->
+                <googlemaps-user-position @update:position="setUserPosition" />
+              </googlemaps-map>
+
+              <!-- Places list -->
+              <googlemaps-nearby-places
+                  class="results-pane"
+                  ref="results"
+                  :request="nearbyRequest"
+              >
+                  <template slot-scope="props">
+                    <v-layout row wrap>
+                        <v-flex
+                          v-for="result of props.results"
+                          :key="result.id"
                         >
-                          <v-layout fill-height>
-                            <v-flex xs12 align-end flexbox>
-                              
-                            </v-flex>
-                          </v-layout>
-                        </v-container>
-                      </v-img>
+                          <v-card>
+                            <v-img
+                              src="http://wallpaperstock.net/the-beatles-minimalistic-illustration_wallpapers_43830_1600x1200.jpg"
+                              height="200px"
+                            >
+                              <v-container
+                                fill-height
+                                fluid
+                                pa-2
+                              >
+                                <v-layout fill-height>
+                                  <v-flex xs12 align-end flexbox>
+                                    <span class="headline white--text">{{ result.name }}</span><br>
+                                    <span class="white--text">{{ result.vicinity }}</span></br>
+                                  </v-flex>
+                                </v-layout>
+                              </v-container>
+                            </v-img>
 
-                      <v-card-actions>
-                        <v-spacer></v-spacer>
-                        <v-btn icon>
-                          <v-icon>favorite</v-icon>
-                        </v-btn>
-                        <v-btn icon>
-                          <v-icon>bookmark</v-icon>
-                        </v-btn>
-                        <v-btn icon>
-                          <v-icon>share</v-icon>
-                        </v-btn>
-                      </v-card-actions>
-                    </v-card>
-                  </v-flex>
-                  <v-flex
-                    v-for="item in shoppingItems"
-                  >
-                    <v-card>
-                      <v-img
-                        src="https://cdn.shopify.com/s/files/1/0434/4749/files/Hazard_Haircut_1_grande.jpg?v=1528904232"
-                        height="200px"
-                      >
-                        <v-container
-                          fill-height
-                          fluid
-                          pa-2
-                        >
-                          <v-layout fill-height>
-                            <v-flex xs12 align-end flexbox>
-
-                            </v-flex>
-                          </v-layout>
-                        </v-container>
-                      </v-img>
-
-                      <v-card-actions>
-                        <v-spacer></v-spacer>
-                        <v-btn icon>
-                          <v-icon>favorite</v-icon>
-                        </v-btn>
-                        <v-btn icon>
-                          <v-icon>bookmark</v-icon>
-                        </v-btn>
-                        <v-btn icon>
-                          <v-icon>share</v-icon>
-                        </v-btn>
-                      </v-card-actions>
-                    </v-card>
-                  </v-flex>
-                </v-layout>
+                            <v-card-actions>
+                              <v-spacer></v-spacer>
+                              <v-btn icon>
+                                <v-icon>favorite</v-icon>
+                              </v-btn>
+                              <v-btn icon>
+                                <v-icon>bookmark</v-icon>
+                              </v-btn>
+                              <v-btn icon>
+                                <v-icon>share</v-icon>
+                              </v-btn>
+                            </v-card-actions>
+                          </v-card>
+                        </v-flex>
+                      </v-layout>
+                      <!-- <LoadingOverlay v-if="props.loading"/> -->
+                  </template>
+              </googlemaps-nearby-places>
               </v-container>
         </v-flex>
        </v-layout>
@@ -101,33 +87,47 @@
   import { mapState } from 'vuex'
 
   export default {
-    data () {
+    data: function () {
         return {
             center: {
-                lat: 48.853,
-                lng: 2.298,
+                lat: null,
+                lng: null
             },
             searchBounds: null,
             userPosition: null,
         }
     },
+    components: {
+      navbar
+    },
     computed: {
+        ...mapGetters([
+            'address',
+            'long',
+            'lat'
+        ]),
         nearbyRequest () {
             if (this.searchBounds) {
                 return {
-                    bounds: this.searchBounds,
+                    bounds: 5000,
                     type: 'hair_care'
                 }
             }
         },
     },
+
+    /*
+     ******************
+     ****IMPORTANT*****
+     ******************
+     * We will start using these methods in the future
+     *
+     */
+
     methods: {
-        ...mapActions('layout', [
-            'toggleSideNav',
-        ]),
         centerOnUser () {
             if (this.userPosition) {
-                this.center = this.userPosition
+              this.center = this.userPosition
             }
         },
         onIdle (map) {
@@ -137,10 +137,25 @@
         setUserPosition (position) {
             this.userPosition = position
         },
+        setCenter () {
+          var center = {
+            lat: this.lat,
+            lng: this.long
+          }
+        }
     },
-    mounted() {
+    
+    mounted: function () {
+        var self = this;
+        this.setCenter() = this.center;
         console.log(this.$refs.results);
     }
+
+  //      mounted: function() {
+  //        this.getTeams().then(data => {
+  //          this.teams = data;
+  //        });
+  //      }
 };
     
   // export default {
@@ -194,5 +209,29 @@
 <style lang="scss" scoped>
   h1 {
     text-align: center;
+  }
+  .demo {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+  }
+  .panes {
+    flex: 1;
+    display: flex;
+    flex-direction: row;
+    align-items: stretch;
+  }
+  .map {
+    flex: 1;
+  }
+  .results-pane {
+    overflow-x: auto;
+    overflow-y: auto;
+    width: 400px;
+    height: calc(100vh - 64px);
+    position: relative;
+    .photo {
+        background: #eee;
+    }
   }
 </style>
