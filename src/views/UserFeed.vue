96b3a9ece70bd16a64b4bf9e028ae7e3ba724970
Werner Kotze <werner.kotze@clevva.com>
2019-11-19 22:45:27 +0200

    started userfeed cleanup

--- src/views/UserFeed.vue
+++ src/views/UserFeed.vue
@@ -1,43 +1,70 @@
 <template>
   <v-app id="inspire">
     <v-content>
-      <v-container fluid fill-height>
+      <v-container fluid>
         <v-row justify-center>
          <v-flex>
               <v-container
                 fluid
               >
-                <v-row column class="max-view" v-if="resultList && randi != -1">
-                  <gmap-map ref="resultmap"
-                    :center="resultList[randi].geometry.location"
-                    :zoom="zoom"
-                    :options="mapOptions"
-                    map-type-id="terrain"
-                    style="width: 100vw; height: 30vh"
-                  >
-                    <gmap-marker :position="resultList[randi].geometry.location"></gmap-marker>
-                    <gmap-marker :position="position" icon="/static/img/streetview-icon.png"></gmap-marker>
-                  </gmap-map>
+                  <v-row>
+                    <v-col cols="12">
+                      <v-row
+                        align="center"
+                        justify="center"
+                        class="grey lighten-5"
+                        style="height: 300px;"
+                      >
+                        <v-card
+                          max-width="344"
+                          class="mx-auto mb-4"
+                          v-for="result of resultList"
+                          :key="result.id"
+                        >
+                          <v-list-item>
+                            <v-list-item-avatar color="grey">
+                              
+                            </v-list-item-avatar>
+                            <v-list-item-content>
+                              <v-list-item-title class="headline">{{ result.name }}</v-list-item-title>
+                              <v-list-item-subtitle>by Kurt Wagner</v-list-item-subtitle>
+                            </v-list-item-content>
+                          </v-list-item>
+                          <!-- 
+                          <v-img
+                            src="https://cdn.vuetifyjs.com/images/cards/mountain.jpg"
+                            height="194"
+                          ></v-img> -->
 
-                  <v-row column class="text" v-if="resultList.length > 1">
-                    <img class="place-img" :src="resultList[randi].photos && resultList[randi].photos[0] ? resultList[randi].photos[0].getUrl({'maxWidth': 256, 'maxHeight': 256}) : '/static/img/default_place.png'" alt="">
-                    <h3 class="teal--text lighten-2">{{resultList[randi].name}}</h3>
-                    <star-rating class="start-rating" :rating="resultList[randi].rating" :read-only="true" :star-size="20" :show-rating="false"></star-rating>
-                    <h5>{{resultList[randi].vicinity}}</h5>
-                  </v-row>
+                          <v-card-text>
+                            Visit ten places on our planet that are undergoing the biggest changes today.
+                          </v-card-text>
 
-                  <v-row column class="text" v-else>
-                    <h1 class="teal--text lighten-2">; - ;</h1>
-                    <h3 class="teal--text lighten-2">Nothing left to eat</h3>
-                    <h5>เรื่องมากชิบหาย</h5>
-                  </v-row>
-
-                  <v-row column class="btn-container">
-                    <v-btn rounded primary dark @click="random" class="pink lighten-2" :disabled="resultList.length <= 1">Re-random</v-btn>
-                    <v-btn rounded dark @click="back" class="blue-grey lighten-1">Back</v-btn>
-                  </v-row>
-
-                </v-row>
+                          <v-card-actions>
+                            <v-btn
+                              text
+                              color="deep-purple accent-4"
+                            >
+                              Read
+                            </v-btn>
+                            <v-btn
+                              text
+                              color="deep-purple accent-4"
+                            >
+                              Bookmark
+                            </v-btn>
+                            <v-spacer></v-spacer>
+                            <v-btn icon>
+                              <v-icon>mdi-heart</v-icon>
+                            </v-btn>
+                            <v-btn icon>
+                              <v-icon>mdi-share-variant</v-icon>
+                            </v-btn>
+                          </v-card-actions>
+                        </v-card>
+                      </v-row>
+                    </v-col>
+                  </v-row>        
               </v-container>
         </v-flex>
        </v-row>
