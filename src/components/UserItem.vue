99e159c5da811da4a35097687b10df047dcd7cfa
Brendan Jacobs <drummer.bren@gmail.com>
2020-02-23 09:07:10 +0200

    minor changes to useritem and userdetail

--- src/components/UserItem.vue
+++ src/components/UserItem.vue
@@ -2,15 +2,15 @@
   <div class="user-item">
     <router-link
       class="user-link"
-      :to="{ name: 'allusers', params: { id: allusers } }"
+      :to="{ name: 'allusers', params: { id: data.id } }"
     >
-      #{{ index }} {{ allusers.id }}
+      #{{ index }} {{ data.displayName }}
     </router-link>
      
     <div
       v-if="!disableActions"
       class="delete-btn"
-      @click="$emit('deleteUser', allusers.id)"
+      @click="$emit('deleteUser', allusers)"
     >
       {{ isUserDeletionPending ? 'delete in progress...' : 'delete' }}
     </div>
@@ -20,7 +20,7 @@
 <script>
 export default {
     props: {
-    allusers: Object,
+    data: Object,
     index: Number,
     isUserDeletionPending: Boolean,
     disableActions: Boolean
--- src/components/UserList.vue
+++ src/components/UserList.vue
@@ -6,11 +6,11 @@
     </p>
     <user-item
      v-for="(user, index) in allusers"
-      :key="user.id"
+      :key="user.index"
       class="user-row"
       :index="index"
       :disable-actions="!networkOnLine"
-      :data="user.id"
+      :data="user"
       @deleteUser="deleteUserDetail"
     ></user-item>
   </div>
@@ -23,7 +23,7 @@ import { mapState, mapActions, mapGetters } from 'vuex'
 export default {
   components: { UserItem },
   computed: {
-    ...mapGetters('users', 'allusers', ['isUserDeletionPending']),
+    ...mapGetters('users', ['isUserDeletionPending']),
     ...mapState('users', ['allusers']),
     ...mapState('app', ['networkOnLine'])
   },
