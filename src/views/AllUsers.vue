6f69b9d4cc16074d20816c2236249f3bebf21bd4
Brendan Jacobs <drummer.bren@gmail.com>
2020-02-23 07:52:25 +0200

    added alluser.vue file

---
+++ src/views/AllUsers.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-app>
+    <nav-bar></nav-bar>
+    <div class="mt-4">
+      <h1 class="products-page-title">All Users page</h1>
+      <user-list class="user-list"></user-list>
+      <add-user v-if="networkOnLine"></add-user>
+    </div>
+  </v-app>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+import UserList from '@/components/UserList'
+import AddUser from '@/components/AddUser'
+import NavBar from '@/components/NavBar'
+
+export default {
+  components: { NavBar, UserList, AddUser },
+  computed: mapState('app', ['networkOnLine'])
+};
+</script>
+
+<style lang="scss" scoped>
+@import '@/theme/variables.scss';
+
+.products-page-title {
+  text-align: center;
+  margin-bottom: 60px;
+}
+
+.product-list {
+  margin: 20px 0;
+}
+</style>
